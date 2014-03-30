d48084f693dc97abfa2c49e0ec19bd59eb8afd72
Brian J Smith <brian-j-smith@uiowa.edu>
2014-03-29 19:33:48 -0500

    Removed gradient function in lieu of the one provided by the Calculus package.

--- src/utils.jl
+++ src/utils.jl
@@ -1,23 +1,5 @@
 #################### Utility Functions ####################
 
-centralrule(x) = cbrt(eps(eltype(x))) * max(one(eltype(x)), abs(x))
-
-function gradient{T<:Real}(f::Function, x::Vector{T}, args...)
-  n = length(x)
-  g = Array(Float64, n)
-  for i in 1:n
-    epsilon = centralrule(x[i])
-    x0 = x[i]
-    x[i] = x0 - epsilon
-    fx1 = f(x, args...)
-    x[i] = x0 + epsilon
-    fx2 = f(x, args...)
-    x[i] = x0
-    g[i] = (fx2 - fx1) / (epsilon + epsilon)
-  end
-  g
-end
-
 invlogit(x) = 1.0 / (exp(-x) + 1.0)
 
 logit(x) = log(x ./ (1.0 - x))
