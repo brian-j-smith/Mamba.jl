8fa9b6ad2d01453a49db2da12261f64b82094b93
Brian J Smith <brian-j-smith@uiowa.edu>
2014-04-03 13:18:18 -0500

    Moved annotate function.

--- src/utils.jl
+++ src/utils.jl
@@ -1,9 +1,5 @@
 #################### Utility Functions ####################
 
-function annotate(x::Matrix, rownames::Vector, colnames::Vector)
-  hcat(["", rownames], vcat(colnames', x))
-end
-
 invlogit(x) = 1.0 / (exp(-x) + 1.0)
 
 logit(x) = log(x ./ (1.0 - x))
