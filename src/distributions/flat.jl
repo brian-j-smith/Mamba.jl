93ff88eb8cf1942140bb013ed6c45bd9a74d3324
Brian J Smith <brian-j-smith@uiowa.edu>
2014-05-24 18:43:36 -0500

    Allow bounded Flat distributions.

--- src/distributions/flat.jl
+++ src/distributions/flat.jl
@@ -9,6 +9,5 @@ insupport(d::Flat, x::Real) = true
 logpdf(d::Flat, x::Real) = 0.0
 
 function Truncated(d::Flat, l::Real, u::Real)
-  isinf(l) || isinf(u) || error("Flat distributions cannot be bounded")
   Truncated{Flat,Continuous}(d, l, u, 1.0)
 end
