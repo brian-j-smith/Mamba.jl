40e0f6b48fb8665d6cc304005cea32c09edff8e4
Benjamin Deonovic <bdeonovic@gmail.com>
2014-06-10 15:15:17 -0500

    Optional legends

--- src/output/plot.jl
+++ src/output/plot.jl
@@ -1,15 +1,16 @@
 function plot(c::MCMCChains, ptype::Symbol=:summary; args...)
-  ptype == :summary ? [traceplot(c) densityplot(c)].' :
-  ptype == :trace   ? traceplot(c) :
-  ptype == :density ? densityplot(c) :
+  ptype == :summary ? [traceplot(c; args...) densityplot(c; args...)].' :
+  ptype == :trace   ? traceplot(c; args...) :
+  ptype == :density ? densityplot(c; args...) :
   ptype == :autocor ? autocorplot(c; args...) :
-  ptype == :mean    ? meanplot(c) :
+  ptype == :mean    ? meanplot(c; args...) :
     error("unsupported plot type $ptype")
 end
 
-function traceplot(c::MCMCChains)
+function traceplot(c::MCMCChains; legend::Bool=false)
   nrows, nvars, nchains = size(c.value)
   plots = Array(Plot, nvars)
+  pos = legend ? :right : :none
   for i in 1:nvars
     plots[i] = plot(y=[[c.value[:,i,j] for j in 1:nchains]...],
                     x=[[c.range for j in 1:nchains]...],
@@ -17,27 +18,29 @@ function traceplot(c::MCMCChains)
                     color=repeat([1:nchains], inner=[length(c.range)]),
                     Scale.discrete_color(), Guide.colorkey("Chain"),
                     Guide.xlabel("Iteration"), Guide.ylabel("Value"),
-                    Guide.title(c.names[i]))
+                    Guide.title(c.names[i]), Theme(key_position=pos))
   end
   return plots
 end
 
-function densityplot(c::MCMCChains)
+function densityplot(c::MCMCChains; legend::Bool=false)
   nrows, nvars, nchains = size(c.value)
   plots = Array(Plot, nvars)
+  pos = legend ? :right : :none
   for i in 1:nvars
     plots[i] = plot(x=[[c.value[:,i,j] for j in 1:nchains]...], Geom.density,
                     color=repeat([1:nchains], inner=[length(c.range)]),
                     Scale.discrete_color(), Guide.colorkey("Chain"),
                     Guide.xlabel("Value"), Guide.ylabel("Density"),
-                    Guide.title(c.names[i]))
+                    Guide.title(c.names[i]), Theme(key_position=pos))
   end
   return plots
 end
 
-function autocorplot(c::MCMCChains; maxlag::Integer=100)
+function autocorplot(c::MCMCChains; maxlag::Integer=100, legend::Bool=false)
   nrows, nvars, nchains = size(c.value)
   plots = Array(Plot, nvars)
+  pos = legend ? :right : :none
   lags = [(0:maxlag) * step(c.range)]
   ac = autocor(c, lags=[0:maxlag])
   for i in 1:nvars
@@ -47,14 +50,15 @@ function autocorplot(c::MCMCChains; maxlag::Integer=100)
                     color=repeat([1:nchains], inner=[maxlag]),
                     Scale.discrete_color(), Guide.colorkey("Chain"),
                     Guide.xlabel("Lag"), Guide.ylabel("Autocorrelation"),
-                    Guide.title(c.names[i]))
+                    Guide.title(c.names[i]), Theme(key_position=pos))
   end
   return plots
 end
 
-function meanplot(c::MCMCChains)
+function meanplot(c::MCMCChains; legend::Bool=false)
   nrows, nvars, nchains = size(c.value)
   plots = Array(Plot, nvars)
+  pos = legend ? :right : :none
   for i in 1:nvars
     plots[i] = plot(y=[[cumsum(c.value[:,i,j])./[1:nrows] for j in 1:nchains]...],
                     x=[[c.range for j in 1:nchains]...],
@@ -62,7 +66,7 @@ function meanplot(c::MCMCChains)
                     color=repeat([1:nchains], inner=[length(c.range)]),
                     Scale.discrete_color(), Guide.colorkey("Chain"),
                     Guide.xlabel("Iteration"), Guide.ylabel("Mean"),
-                    Guide.title(c.names[i]))
+                    Guide.title(c.names[i]), Theme(key_position=pos))
   end
   return plots
 end
