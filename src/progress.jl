701b17fbc760ca71bdb7cc9db31ec3222b3b1fac
Brian J Smith <brian-j-smith@uiowa.edu>
2014-07-05 16:57:29 -0500

    Report remaining and total time.

--- src/progress.jl
+++ src/progress.jl
@@ -4,30 +4,40 @@ type ChainProgress
   chain::Integer
   iters::Integer
   counter::Integer
+  runin::Integer
+  threshold::Float64
+  t0::Float64
 
-  ChainProgress(chain::Integer, iters::Integer) = new(chain, iters, 0)
+  function ChainProgress(chain::Integer, iters::Integer)
+    new(chain, iters, 0, max(1, min(10, iround(0.01 * iters))), 0.0, time())
+  end
 end
 
 
 #################### ChainProgress Methods ####################
 
 function next!(p::ChainProgress)
-  counter = p.counter
   p.counter += 1
-
-  dt = 10.0 / p.iters
-  if floor(p.counter * dt) > counter * dt || counter == 0
+  if p.counter / p.iters >= p.threshold && p.counter >= p.runin
+    p.threshold += 0.10
     print(STDOUT, p)
   end
-
   p
 end
 
 function Base.print(io::IO, p::ChainProgress)
-  pct = iround(100.0 * p.counter / p.iters)
-  str = string(
-    "Chain ", p.chain, ": ", lpad(p.counter, length(string(p.iters)), ' '),
-    "/$(p.iters) [", lpad(pct, 3, ' '), "%] @ $(strftime(time()))"
-  )
+  elapsed = time() - p.t0
+  remaining = elapsed * (p.iters / p.counter - 1.0)
+  str = @sprintf("Chain %u: %3u%% [%s of %s remaining]",
+                 p.chain,
+                 100.0 * p.counter / p.iters,
+                 strfsec(remaining),
+                 strfsec(elapsed + remaining))
   println(io, str)
+end
+
+function strfsec(sec::Real)
+  hr, sec = divrem(sec, 3600)
+  min, sec = divrem(sec, 60)
+  @sprintf "%u:%02u:%02u" hr min sec
 end
