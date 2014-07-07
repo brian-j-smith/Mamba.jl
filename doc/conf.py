843dcceaf7251ca4981e3fb6f88d25d9d1b2d127
Brian J Smith <brian-j-smith@uiowa.edu>
2014-07-07 09:30:26 -0500

    Update documentation main version to 0.2.

--- doc/conf.py
+++ doc/conf.py
@@ -63,7 +63,7 @@ copyright = '2014, Brian J Smith'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1'
+version = '0.2'
 # The full version, including alpha/beta/rc tags.
 release = '0.2.0'
 
