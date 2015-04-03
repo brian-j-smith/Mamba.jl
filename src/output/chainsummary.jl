01a0d9f4e3f265208b0a667622f22bf601e478de
Douglas Bates <dmbates@gmail.com>
2015-04-03 13:35:15 -0500

    Modify the annotate method to use showoff.
    I would prefer that what is now annotate would become a show method.

--- src/output/chainsummary.jl
+++ src/output/chainsummary.jl
@@ -35,20 +35,56 @@ end
 
 #################### ChainSummary Base Methods ####################
 
-function Base.show(io::IO, s::ChainSummary)
-  Base.showlimited(annotate(s.value, s.rownames, s.colnames))
-  println(io)
-end
-
 Base.showall(s::ChainSummary, header::Bool=true) = showall(STDOUT, s, header)
 
 function Base.showall(io::IO, s::ChainSummary, header::Bool=true)
   !header || println(io, s.header)
   Base.with_output_limit(true) do
-    Base.showarray(io, annotate(s.value, s.rownames, s.colnames), limit=false)
+    Base.showarray(io, split(annotate(s), ['\n']); limit=false)
   end
   println(io)
 end
+
+
+Base.show(io::IO, s::ChainSummary) = print(io, annotate(s))
+
+# write n ' ' characters to io
+wrtsp(io::IO, n) = while (n -= 1) >= 0 write(io, ' ') end
+
+## return a character string suitable for printing
+function annotate(s::ChainSummary)
+  io = IOBuffer()
+  rnwid = map(length,s.rownames)   # rowname widths
+  mxrnwid = maximum(rnwid)
+  cnwid = map(length,s.colnames)   # column name widths
+  charv = mapslices(showoff, s.value, 1) # s.value as right alignable strings
+  colwid = 1 + max(cnwid,vec(maximum(map(length,charv),[1,3])))
+  m,n,f = size(charv)
+  for k in 1:f
+    ## write the column headers centered on the column widths
+    wrtsp(io, mxrnwid)
+    for j in 1:n
+      nspace = colwid[j] - cnwid[j] - 1 # don't count the leading space
+      nright = nspace >> 1              # divide by 2 rounding down
+      wrtsp(io, 1 + nspace - nright)
+      print(io, s.colnames[j])
+      wrtsp(io, nright)
+    end
+    println(io)
+    for i in 1:m
+      wrtsp(io, mxrnwid - rnwid[i])
+      print(io, s.rownames[i])
+      for j in 1:n
+        wrtsp(io, colwid[j] - length(charv[i,j,k]))
+        print(io, charv[i,j,k])
+      end
+      println(io)
+    end
+    println(io)
+  end
+  bytestring(io)
+end
+
 
 function annotate{T}(x::Array{T,3}, rownames::Vector, colnames::Vector)
   if size(x, 3) == 1
