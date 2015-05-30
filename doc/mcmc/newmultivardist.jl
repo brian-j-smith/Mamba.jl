9dc3b5f9bfeea2f42f7a63709550a18f656fbff5
Brian J Smith <brian-j-smith@uiowa.edu>
2015-05-30 15:57:19 -0500

    Add documentation for user-defined distributions.

---
+++ doc/mcmc/newmultivardist.jl
@@ -0,0 +1,104 @@
+## Define a new multivariate Distribution type for Mamba.
+## The definition must be placed within an unevaluated quote block.
+@everywhere extensions = quote
+
+  ## Load packages and functions needed for the definition
+  using Distributions
+  import Distributions: _logpdf, length
+
+  ## Type declaration
+  type NewMultivarDist <: ContinuousMultivariateDistribution
+    mu::Vector{Float64}
+    sigma::Float64
+
+    ## Constructor
+    function NewMultivarDist(mu, sigma)
+      new(convert(Vector{Float64}, mu), convert(Float64, sigma))
+    end
+  end
+
+  ## The following method functions must be implemented
+
+  ## Dimension of the distribution
+  length(d::NewMultivarDist) = length(d.mu)
+
+  ## Minimum and maximum support values
+  minimum(d::NewMultivarDist) = -Inf
+  maximum(d::NewMultivarDist) = Inf
+
+  ## Logicals indicating whether elements of x are in the support
+  function insupport{T<:Real}(d::NewMultivarDist, x::Vector{T})
+    minimum(d) .<= x .<= maximum(d)
+  end
+
+  ## Normalized or unnormalized log-density value
+  function _logpdf{T<:Real}(d::NewMultivarDist, x::Vector{T})
+    -length(x) * log(d.sigma) - 0.5 * sumabs2(x - d.mu) / d.sigma^2
+  end
+
+end
+
+## Test the extensions in a temporary module (optional)
+module Testing end
+eval(Testing, extensions)
+d = Testing.NewMultivarDist([0.0, 0.0], 1.0)
+Testing.minimum(d)
+Testing.maximum(d)
+Testing.insupport(d, [2.0, 3.0])
+Testing.logpdf(d, [2.0, 3.0])
+
+## Add the extensions to Mamba
+using Mamba
+@everywhere eval(Mamba, extensions)
+
+## Implement a Mamba model using the new distribution
+model = Model(
+
+  y = Stochastic(1,
+    @modelexpr(mu, s2,
+      NewMultivarDist(mu, sqrt(s2))
+    ),
+    false
+  ),
+
+  mu = Logical(1,
+    @modelexpr(xmat, beta,
+      xmat * beta
+    ),
+    false
+  ),
+
+  beta = Stochastic(1,
+    :(MvNormal(2, sqrt(1000)))
+  ),
+
+  s2 = Stochastic(
+    :(InverseGamma(0.001, 0.001))
+  )
+
+)
+
+## Sampling Scheme
+scheme = [NUTS([:beta]),
+          Slice([:s2], [3.0])]
+
+## Sampling Scheme Assignment
+setsamplers!(model, scheme)
+
+## Data
+line = (Symbol => Any)[
+  :x => [1, 2, 3, 4, 5],
+  :y => [1, 3, 3, 3, 5]
+]
+line[:xmat] = [ones(5) line[:x]]
+
+## Initial Values
+inits = Dict{Symbol,Any}[
+  [:y => line[:y],
+   :beta => rand(Normal(0, 1), 2),
+   :s2 => rand(Gamma(1, 1))]
+  for i in 1:3]
+
+## MCMC Simulation
+sim = mcmc(model, line, inits, 10000, burnin=250, thin=2, chains=3)
+describe(sim)
---
+++ doc/mcmc/newunivardist.jl
@@ -0,0 +1,103 @@
+## Define a new univariate Distribution type for Mamba.
+## The definition must be placed within an unevaluated quote block.
+@everywhere extensions = quote
+
+  ## Load packages needed for the definition
+  using Distributions
+
+  ## Type declaration
+  type NewUnivarDist <: ContinuousUnivariateDistribution
+    mu::Float64
+    sigma::Float64
+
+    ## Constructor
+    function NewUnivarDist(mu, sigma)
+      new(convert(Float64, mu), convert(Float64, sigma))
+    end
+  end
+
+  ## The following method functions must be implemented
+
+  ## Minimum and maximum support values
+  minimum(d::NewUnivarDist) = -Inf
+  maximum(d::NewUnivarDist) = Inf
+
+  ## Logical indicating whether x is in the support
+  function insupport(d::NewUnivarDist, x::Real)
+    minimum(d) <= x <= maximum(d)
+  end
+
+  ## Normalized or unnormalized log-density value
+  function logpdf(d::NewUnivarDist, x::Real)
+    -log(d.sigma) - 0.5 * ((x - d.mu) / d.sigma)^2
+  end
+
+end
+
+## Test the extensions in a temporary module (optional)
+module Testing end
+eval(Testing, extensions)
+d = Testing.NewUnivarDist(0.0, 1.0)
+Testing.minimum(d)
+Testing.maximum(d)
+Testing.insupport(d, 2.0)
+Testing.logpdf(d, 2.0)
+
+## Add the extensions to Mamba
+using Mamba
+@everywhere eval(Mamba, extensions)
+
+## Implement a Mamba model using the new distribution
+model = Model(
+
+  y = Stochastic(1,
+    @modelexpr(mu, s2,
+      begin
+        sigma = sqrt(s2)
+        Distribution[NewUnivarDist(mu[i], sigma) for i in 1:length(mu)]
+      end
+    ),
+    false
+  ),
+
+  mu = Logical(1,
+    @modelexpr(xmat, beta,
+      xmat * beta
+    ),
+    false
+  ),
+
+  beta = Stochastic(1,
+    :(MvNormal(2, sqrt(1000)))
+  ),
+
+  s2 = Stochastic(
+    :(InverseGamma(0.001, 0.001))
+  )
+
+)
+
+## Sampling Scheme
+scheme = [NUTS([:beta]),
+          Slice([:s2], [3.0])]
+
+## Sampling Scheme Assignment
+setsamplers!(model, scheme)
+
+## Data
+line = (Symbol => Any)[
+  :x => [1, 2, 3, 4, 5],
+  :y => [1, 3, 3, 3, 5]
+]
+line[:xmat] = [ones(5) line[:x]]
+
+## Initial Values
+inits = Dict{Symbol,Any}[
+  [:y => line[:y],
+   :beta => rand(Normal(0, 1), 2),
+   :s2 => rand(Gamma(1, 1))]
+  for i in 1:3]
+
+## MCMC Simulation
+sim = mcmc(model, line, inits, 10000, burnin=250, thin=2, chains=3)
+describe(sim)
