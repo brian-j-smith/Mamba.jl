ccf1742bfa65a9aa4640988d4b09537bb759534f
Brian J Smith <brian-j-smith@uiowa.edu>
2015-09-09 21:35:21 -0500

    Standardize BDS types and sampler behavior.

--- src/samplers/bds.jl
+++ src/samplers/bds.jl
@@ -1,43 +1,63 @@
-##################### Binary Deterministic Sampler ##################
+#################### Binary Deterministic Sampler ####################
 
-######################### Sampler Constructor #######################
+#################### Types ####################
 
-function BDS{T <: Integer}(params::Vector{Symbol}, d::T, 
-                           Γ::Vector{Vector{T}}=[[i] for i in 1:d])
-  # If user supplies empty Γ, 
-  # sample number of components for index set
-  if size(Γ,1) == 0
-    G = Truncated(Geometric(2/d), 1, d)
-    k = rand(G)
-    Γ = collect(combinations(1:d, k))
-  end
+type BDSTune
+  Γ::Vector{Vector{Int}}
+end
 
+type BDSVariate <: VectorVariate
+  value::Vector{Float64}
+  tune::BDSTune
+
+  BDSVariate(x::Vector{Float64}, tune::BDSTune) = new(x, tune)
+end
+
+function BDSVariate(x::Vector{Float64}, tune=nothing)
+  tune = BDSTune(
+    Vector{Vector{Int}}[]
+  )
+  BDSVariate(x, tune)
+end
+
+
+#################### Sampler Constructor ####################
+
+function BDS(params::Vector{Symbol}, d::Integer)
+  G = Truncated(Geometric(2 / d), 1, d)
+  k = Int(rand(G))
+  Γ = collect(combinations([1:d;], k))
+  BDS(params, Γ)
+end
+
+function BDS(params::Vector{Symbol}, Γ::Vector{Vector{Int}})
   Sampler(params,
-  quote
-    x = round(Int,unlist(model,block,true))
-    tunepar = tune(model,block)
-    f = y -> logpdf!(model,y,block,true)
-    bds!(x,tunepar["Γ"],f)
-    relist(model,x,block,true)
-  end,
-  Dict("Γ" => Γ)
+    quote
+      x = unlist(model, block, false)
+      tunepar = tune(model, block)
+      f = y -> logpdf!(model, y, block, false)
+      v = BDSVariate(x)
+      bds!(v, tunepar["Γ"], f)
+      relist(model, v.value, block, false)
+    end,
+    Dict("Γ" => Γ)
   )
 end
 
-########################## Sampling Functions #######################
 
-function bds!{T <: Integer}(x::Vector{T}, Γ::Vector{Vector{T}},
-                            logf::Function)
-  #proposal, initialize to current state
-  y = x[:]
+#################### Sampling Functions ####################
 
-  #indices to flip
+function bds!(v::BDSVariate, Γ::Vector{Vector{Int}}, logf::Function)
+  x = v[:]
+
+  # sample indices and flip values
   idx = Γ[rand(1:length(Γ))]
+  x[idx] = 1.0 - v[idx]
 
-  y[idx] = 1 - x[idx]
-
-  lr = logf(y) - logf(x)
-  if rand() < exp(lr)
-    x[:] = y
+  if rand() < exp(logf(x) - logf(v.value))
+    v[:] = x
   end
+  v.tune.Γ = Γ
+
+  v
 end
