963448514e62334a087927a3c87d65c35d9cbe1a
Benjamin Deonovic <bdeonovic@gmail.com>
2015-10-14 12:47:12 -0500

    MALA Sampler

---
+++ doc/samplers/mala.jl
@@ -0,0 +1,45 @@
+################################################################################
+## Linear Regression
+##   y ~ N(b0 + b1 * x, s2)
+##   b0, b1 ~ N(0, 1000)
+##   s2 ~ invgamma(0.001, 0.001)
+################################################################################
+
+using Mamba
+
+## Data
+data = Dict(
+  :x => [1, 2, 3, 4, 5],
+  :y => [1, 3, 3, 3, 5]
+)
+
+## Log-transformed Posterior(b0, b1, log(s2)) + Constant and Gradient Vector
+fx = function(x)
+  b0 = x[1]
+  b1 = x[2]
+  logs2 = x[3]
+  r = data[:y] - b0 - b1 * data[:x]
+  logf = (-0.5 * length(data[:y]) - 0.001) * logs2 -
+           (0.5 * dot(r, r) + 0.001) / exp(logs2) -
+           0.5 * b0^2 / 1000 - 0.5 * b1^2 / 1000
+  grad = [
+    sum(r) / exp(logs2) - b0 / 1000,
+    sum(data[:x] .* r) / exp(logs2) - b1 / 1000,
+    -0.5 * length(data[:y]) - 0.001 + (0.5 * dot(r, r) + 0.001) / exp(logs2)
+  ]
+  logf, grad
+end
+
+## MCMC Simulation with Metropolis-adjusted Langevin algorithm
+n = 5000
+burnin = 1000
+sim = Chains(n, 3, start = (burnin + 1), names = ["b0", "b1", "s2"])
+theta = MALAVariate([0.0, 0.0, 0.0])
+epsilon = 0.01
+for i in 1:n
+  mala!(theta, epsilon, cholfact(eye(3)), fx)
+  if i > burnin
+    sim[i,:,1] = [theta[1:2]; exp(theta[3])]
+  end
+end
+describe(sim)
---
+++ src/samplers/mala.jl
@@ -0,0 +1,83 @@
+#################### Metropolis Adjusted Langevin Algorithm ####################
+
+#################### Types ####################
+
+type MALATune
+  U::Cholesky{Float64}
+  M::Matrix{Float64}
+  scale::Float64
+end
+
+type MALAVariate <: VectorVariate
+  value::Vector{Float64}
+  tune::MALATune
+
+  MALAVariate(x::Vector{Float64}, tune::MALATune) = new(x, tune)
+end
+
+function MALAVariate(x::Vector{Float64}, tune=nothing)
+  tune = MALATune(
+    Cholesky(Array(Float64, 0, 0), :U),
+    Array(Float64, 0, 0),
+    NaN
+  )
+  MALAVariate(x, tune)
+end
+
+
+#################### Sampler Constructor ####################
+
+function MALA(params::Vector{Symbol}, scale::Float64, M::Matrix{Float64}, dtype::Symbol=:forward)
+  Sampler(params,
+    quote
+      tunepar = tune(model, block)
+      x = unlist(model, block, true)
+      v = MALAVariate(x, tunepar["sampler"])
+      fx = x -> malafx!(model, x, block, tunepar["dtype"])
+      
+      mala!(v, tunepar["scale"], tunepar["U"], fx)
+      tunepar["sampler"] = v.tune
+      relist(model, v.value, block, true)
+    end,
+    Dict("scale" => scale, "U" => cholfact(M), "dtype" => dtype, "sampler" => nothing)
+  )
+end
+
+function malafx{T<:Real}(m::Model, x::Vector{T}, block::Integer, dtype::Symbol)
+  logf = logpdf(m, x, block, true)
+  grad = isfinite(logf) ?
+           gradlogpdf(m, x, block, true, dtype=dtype) :
+           zeros(length(x))
+  logf, grad
+end
+
+function malafx!{T<:Real}(m::Model, x::Vector{T}, block::Integer, dtype::Symbol)
+  logf = logpdf!(m, x, block, true)
+  grad = isfinite(logf) ?
+           gradlogpdf!(m, x, block, true, dtype=dtype) :
+           zeros(length(x))
+  logf, grad
+end
+
+export malafx, malafx!
+
+#################### Sampling Functions ####################
+
+function mala!(v::MALAVariate, scale::Float64, U::Cholesky{Float64}, fx::Function)
+  v.tune.scale = scale
+  v.tune.U = U
+  M = U[:L]*U[:L]'
+
+  logf0, grad0 = fx(v.value)
+  y = v + 0.5 * scale^2 * M * grad0 + scale * U[:L] * randn(length(v))
+
+  logf1, grad1 = fx(y)
+
+  q0 = logpdf(MvNormal(v + 0.5 * scale^2 * grad0, scale), y)
+  q1 = logpdf(MvNormal(y + 0.5 * scale^2 * grad1, scale), v)
+
+  if rand() < exp( (logf1 + q1) - (logf0 + q0))
+    v[:] = y
+  end
+  v
+end
