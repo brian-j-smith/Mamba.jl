04d71f88929c8fea8b380a865cabb53bbbd1221d
Brian J Smith <brian-j-smith@uiowa.edu>
2015-10-15 09:20:00 -0500

    Consolidate distribution constructors.

--- src/distributions/constructors.jl
+++ src/distributions/constructors.jl
@@ -4,7 +4,7 @@
 
 #################### Categorical ####################
 
-Distributions.Categorical(p::VectorVariate) =
+Distributions.Categorical(p::AbstractVector) =
   Categorical(convert(Vector{Float64}, p))
 
 
@@ -14,114 +14,74 @@ Distributions.Categorical(p::VectorVariate) =
 
 #################### Dirichlet ####################
 
-Dirichlet(alpha::VectorVariate) =
+Dirichlet(alpha::AbstractVector) =
   Dirichlet(convert(Vector{Float64}, alpha))
 
 
 #################### Multinomial ####################
 
-Multinomial(n::Integer, p::VectorVariate) =
-  Multinomial(n, convert(Vector{Float64}, p))
+Multinomial(n::Real, p::AbstractVector) =
+  Multinomial(convert(Int, n), convert(Vector{Float64}, p))
 
 
 #################### MvNormal ####################
 
-MvNormal(μ::VectorVariate, Σ::Matrix{Float64}) =
-  MvNormal(convert(Vector{Float64}, μ), Σ)
-MvNormal(μ::Vector{Float64}, Σ::MatrixVariate) =
-  MvNormal(μ, convert(Matrix{Float64}, Σ))
-MvNormal(μ::VectorVariate, Σ::MatrixVariate) =
+MvNormal(μ::AbstractVector, Σ::AbstractMatrix) =
   MvNormal(convert(Vector{Float64}, μ), convert(Matrix{Float64}, Σ))
 
-MvNormal(μ::VectorVariate, σ::Vector{Float64}) =
-  MvNormal(convert(Vector{Float64}, μ), σ)
-MvNormal(μ::Vector{Float64}, σ::VectorVariate) =
-  MvNormal(μ, convert(Vector{Float64}, σ))
-MvNormal(μ::VectorVariate, σ::VectorVariate) =
+MvNormal(μ::AbstractVector, σ::AbstractVector) =
   MvNormal(convert(Vector{Float64}, μ), convert(Vector{Float64}, σ))
 
-MvNormal(μ::VectorVariate, σ::Real) =
-  MvNormal(convert(Vector{Float64}, μ), σ)
+MvNormal(μ::AbstractVector, σ::Real) =
+  MvNormal(convert(Vector{Float64}, μ), convert(Float64, σ))
 
-MvNormal(Σ::MatrixVariate) =
+MvNormal(Σ::AbstractMatrix) =
   MvNormal(convert(Matrix{Float64}, Σ))
 
-MvNormal(σ::VectorVariate) =
+MvNormal(σ::AbstractVector) =
   MvNormal(convert(Vector{Float64}, σ))
 
 
 #################### MvNormalCanon ####################
 
-MvNormalCanon(h::VectorVariate, J::Matrix{Float64}) =
-  MvNormalCanon(convert(Vector{Float64}, h), J)
-MvNormalCanon(h::Vector{Float64}, J::MatrixVariate) =
-  MvNormalCanon(h, convert(Matrix{Float64}, J))
-MvNormalCanon(h::VectorVariate, J::MatrixVariate) =
+MvNormalCanon(h::AbstractVector, J::AbstractMatrix) =
   MvNormalCanon(convert(Vector{Float64}, h), convert(Matrix{Float64}, J))
 
-MvNormalCanon(h::VectorVariate, prec::Vector{Float64}) =
-  MvNormalCanon(convert(Vector{Float64}, h), prec)
-MvNormalCanon(h::Vector{Float64}, prec::VectorVariate) =
-  MvNormalCanon(h, convert(Vector{Float64}, prec))
-MvNormalCanon(h::VectorVariate, prec::VectorVariate) =
+MvNormalCanon(h::AbstractVector, prec::AbstractVector) =
   MvNormalCanon(convert(Vector{Float64}, h), convert(Vector{Float64}, prec))
 
-MvNormalCanon(h::VectorVariate, prec::Float64) =
-  MvNormalCanon(convert(Vector{Float64}, h), prec)
-MvNormalCanon(h::Vector{Float64}, prec::ScalarVariate) =
-  MvNormalCanon(h, convert(Float64, prec))
-MvNormalCanon(h::VectorVariate, prec::ScalarVariate) =
+MvNormalCanon(h::AbstractVector, prec::Real) =
   MvNormalCanon(convert(Vector{Float64}, h), convert(Float64, prec))
 
-MvNormalCanon(J::MatrixVariate) =
+MvNormalCanon(J::AbstractMatrix) =
   MvNormalCanon(convert(Matrix{Float64}, J))
 
-MvNormalCanon(prec::VectorVariate) =
+MvNormalCanon(prec::AbstractVector) =
   MvNormalCanon(convert(Vector{Float64}, prec))
 
-MvNormalCanon(d::Int, prec::ScalarVariate) =
-  MvNormalCanon(d, convert(Float64, prec))
+MvNormalCanon(d::Real, prec::Real) =
+  MvNormalCanon(convert(Int, d), convert(Float64, prec))
 
 
 #################### MvTDist ####################
 
-MvTDist(df::ScalarVariate, μ::Vector{Float64}, C::PDMat) =
-  MvTDist(convert(Float64, df), μ, C)
-MvTDist(df::Float64, μ::VectorVariate, C::PDMat) =
-  MvTDist(df, convert(Vector{Float64}, μ), C)
-MvTDist(df::ScalarVariate, μ::VectorVariate, C::PDMat) =
-  MvTDist(convert(Float64, df), convert(Vector{Float64}, C))
+MvTDist(df::Real, μ::AbstractVector, C::PDMat) =
+  MvTDist(convert(Float64, df), convert(Vector{Float64}, μ), C)
 
-MvTDist(df::ScalarVariate, C::PDMat) =
+MvTDist(df::Real, C::PDMat) =
   MvTDist(convert(Float64, df), C)
 
-MvTDist(df::ScalarVariate, μ::Vector{Float64}, Σ::Matrix{Float64}) =
-  MvTDist(convert(Float64, df), μ, Σ)
-MvTDist(df::Float64, μ::VectorVariate, Σ::Matrix{Float64}) =
-  MvTDist(df, convert(Vector{Float64}, μ), Σ)
-MvTDist(df::Float64, μ::Vector{Float64}, Σ::MatrixVariate) =
-  MvTDist(df, μ, convert(Matrix{Float64}, Σ))
-MvTDist(df::ScalarVariate, μ::VectorVariate, Σ::Matrix{Float64}) =
-  MvTDist(convert(Float64, df), convert(Vector{Float64}, μ), Σ)
-MvTDist(df::ScalarVariate, μ::Vector{Float64}, Σ::MatrixVariate) =
-  MvTDist(convert(Float64, df), μ, convert(Matrix{Float64}, Σ))
-MvTDist(df::Float64, μ::VectorVariate, Σ::MatrixVariate) =
-  MvTDist(df, convert(Vector{Float64}, μ), convert(Matrix{Float64}, Σ))
-MvTDist(df::ScalarVariate, μ::VectorVariate, Σ::MatrixVariate) =
+MvTDist(df::Real, μ::AbstractVector, Σ::AbstractMatrix) =
   MvTDist(convert(Float64, df), convert(Vector{Float64}, μ), convert(Matrix{Float64}, Σ))
 
-MvTDist(df::ScalarVariate, Σ::Matrix{Float64}) =
-  MvTDist(convert(Float64, df), Σ)
-MvTDist(df::Float64, Σ::MatrixVariate) =
-  MvTDist(df, convert(Matrix{Float64}, Σ))
-MvTDist(df::ScalarVariate, Σ::MatrixVariate) =
+MvTDist(df::Real, Σ::AbstractMatrix) =
   MvTDist(convert(Float64, df), convert(Matrix{Float64}, Σ))
 
 
 #################### VonMisesFisher ####################
 
-VonMisesFisher(μ::VectorVariate, κ::Real) =
-  VonMisesFisher(convert(Vector{Float64}, μ), κ)
+VonMisesFisher(μ::AbstractVector, κ::Real) =
+  VonMisesFisher(convert(Vector{Float64}, μ), convert(Float64, κ))
 
 
 ######################################################################
@@ -130,19 +90,11 @@ VonMisesFisher(μ::VectorVariate, κ::Real) =
 
 #################### InverseWishart ####################
 
-InverseWishart(df::ScalarVariate, Ψ::Matrix{Float64}) =
-  InverseWishart(convert(Float64, df), Ψ)
-InverseWishart(df::Real, Ψ::MatrixVariate) =
-  InverseWishart(df, convert(Matrix{Float64}, Ψ))
-InverseWishart(df::ScalarVariate, Ψ::MatrixVariate) =
+InverseWishart(df::Real, Ψ::AbstractMatrix) =
   InverseWishart(convert(Float64, df), convert(Matrix{Float64}, Ψ))
 
 
 #################### Wishart ####################
 
-Wishart(df::ScalarVariate, S::Matrix{Float64}) =
-  Wishart(convert(Float64, df), S)
-Wishart(df::Real, S::MatrixVariate) =
-  Wishart(df, convert(Matrix{Float64}, S))
-Wishart(df::ScalarVariate, S::MatrixVariate) =
+Wishart(df::Real, S::AbstractMatrix) =
   Wishart(convert(Float64, df), convert(Matrix{Float64}, S))
--- src/distributions/mvnormal.jl
+++ src/distributions/mvnormal.jl
@@ -11,12 +11,8 @@ function BDiagNormal(μ::Vector{Float64}, Σ::Vector{Matrix{Float64}})
   MvNormal(μ, PBDiagMat(Σ))
 end
 
-BDiagNormal(μ::VectorVariate, Σ::Matrix{Float64}) =
-  BDiagNormal(convert(Vector{Float64}, μ), Σ)
-BDiagNormal(μ::Vector{Float64}, Σ::MatrixVariate) =
-  BDiagNormal(μ, convert(Matrix{Float64}, Σ))
-BDiagNormal(μ::VectorVariate, Σ::MatrixVariate) =
+BDiagNormal(μ::AbstractVector, Σ::AbstractMatrix) =
   BDiagNormal(convert(Vector{Float64}, μ), convert(Matrix{Float64}, Σ))
 
-BDiagNormal(μ, Σ::Vector) =
+BDiagNormal(μ::AbstractVector, Σ::AbstractVector) =
   BDiagNormal(convert(Vector{Float64}, μ), Matrix{Float64}[Σ...])
