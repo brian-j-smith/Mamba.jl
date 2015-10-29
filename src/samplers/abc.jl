e3d1f7f32c4a0950d63330a5ee08ec15da96c7d8
Benjamin Deonovic <bdeonovic@gmail.com>
2015-10-29 18:02:43 -0500

    ABC Sampler

---
+++ src/samplers/abc.jl
@@ -0,0 +1,141 @@
+type ABCTune
+  Told::Matrix{Float64}
+  epsilon::Vector{Float64}
+  terminals::Vector{Symbol}
+end
+
+function ABC(params::Vector{Symbol}, sigma::Real, summarize::Vector{Function},
+             epsilon::Real; rho::Function = (x, y) -> sqrt(sum((x - y) .^ 2)),
+             s::Int = 1, n::Int = 50, kernel::Symbol = :uniform, adapt::Symbol = :dynamic)
+  kernel in [:uniform, :epanechnikov, :gaussian] ||
+    throw(ArgumentError("kernel must be one of :uniform, 
+    :epanechnikov, or :gaussian"))
+
+  adapt in [:dynamic, :augment] ||
+    throw(ArgumentError("adapt must be one of :dynamic or :augment"))
+
+  Sampler(params,
+    quote
+      tunepar = tune(model, block)
+      params = keys(model, :block, block)
+
+      #previous value
+      x = unlist(model, block, true)
+      
+      #logprior
+      logprior = x -> begin
+        value = 0.0
+        relist!(model, x, block, true)
+        for key in params
+          value += logpdf(model[key], true)
+        end
+        return value
+      end
+
+      T0 = NaN
+      Told = NaN
+      Tnew = NaN
+
+      if tunepar["sampler"] == nothing 
+        terminals = keys(model, :output)
+
+        #data
+        d = unlist(model, terminals)
+
+        #summary stat of data
+        T0 = vcat(map(f -> f(d), tunepar["summarize"])...)
+
+        m = length(T0)
+
+        Told = zeros(m, tunepar["s"])
+        eps0 = zeros(tunepar["s"])
+        for i in 1:tunepar["s"]
+          x0 =  vcat(map(d -> unlist(model[d], 
+                  rand(model[d])), terminals)...)
+          Told[:, i] = vcat(map(f -> f(x0), tunepar["summarize"])...)
+          eps0[i] = tunepar["adapt"] == :dynamic ? 
+            tunepar["rho"](Told[:, i], T0) : Inf
+        end
+        tunepar["sampler"] = ABCTune(Told, eps0, terminals)
+      else
+        #data
+        d = unlist(model, tunepar["sampler"].terminals)
+
+        #summary stat of data
+        T0 = vcat(map(f -> f(d), tunepar["summarize"])...)
+        Told = tunepar["sampler"].Told
+      end
+
+      for k in 1:tunepar["n"]
+        #proposal
+        y = x + tunepar["sigma"] * randn(length(x))
+        relist!(model, y, block, true)
+
+        ratio_num = 0.0
+        ratio_den = 0.0
+
+        Tnew = zeros(length(T0), tunepar["s"])
+        #epsilon = zeros(tunepar["s"])
+        epsilon = fill(Inf, tunepar["s"])
+
+        logeps1 = 0.0
+        logeps0 = 0.0
+
+        for i in 1:tunepar["s"]
+          #new data
+          dnew = vcat(map(d -> unlist(model[d], 
+                   rand(model[d])), tunepar["sampler"].terminals)...)
+
+          #summary stat of new data
+          Tnew[:, i] = vcat(map(f -> f(dnew), tunepar["summarize"])...)
+
+          #epsilon
+          if tunepar["adapt"] == :dynamic
+            epsilon[i] = max(tunepar["epsilon"], min(tunepar["rho"](Tnew[:, i], 
+              T0), tunepar["sampler"].epsilon[i]))
+          elseif model.iter > 1
+            epsilon[i] = randexp() * tunepar["epsilon"] 
+          else
+            epsilon[i] = tunepar["sampler"].epsilon[i]
+          end
+
+          #weighting density
+          rho0 = 0
+          rho1 = 0
+          if tunepar["kernel"] == :uniform
+            rho0 = pdf(Uniform(0, epsilon[i]), tunepar["rho"](Told[:, i], T0))
+            rho1 = pdf(Uniform(0, epsilon[i]), tunepar["rho"](Tnew[:, i], T0))
+          elseif tunepar["kernel"] == :epanechnikov
+            rho0 = prod([pdf(Epanechnikov(Told[j, i], epsilon[i]), T0[i]) 
+              for j in 1:length(T0)])
+            rho1 = prod([pdf(Epanechnikov(Tnew[j, i], epsilon[i]), T0[i]) 
+              for j in 1:length(T0)])
+          elseif tunepar["kernel"] == :gaussian
+            rho0 = pdf(MvNormal(collect(Told[:, i]), epsilon[i]), collect(T0))
+            rho1 = pdf(MvNormal(collect(Tnew[:, i]), epsilon[i]), collect(T0))
+          end
+
+          ratio_num += rho1
+          ratio_den += rho0
+        end
+
+        logeps1 = sum(logpdf(Exponential(tunepar["epsilon"]), epsilon))
+        logeps0 = sum(logpdf(Exponential(tunepar["epsilon"]), tunepar["sampler"].epsilon))
+
+
+        #Reject/Accept
+        if rand() < ratio_num / ratio_den * exp(logprior(y) - logprior(x) + logeps1 - logeps0)
+          x[:] = y
+          tunepar["sampler"].Told = Tnew
+          tunepar["sampler"].epsilon = epsilon
+          break
+        end
+      end
+      relist(model, x, block, true)
+    end,
+    Dict("summarize" => summarize, "rho" => rho, "n" => convert(Int64, n), 
+         "s" => convert(Int64, s), "epsilon" => convert(Float64, epsilon), 
+         "sigma" => convert(Float64, sigma), "kernel" => kernel, 
+         "adapt" => adapt, "sampler" => nothing)
+    )
+end
