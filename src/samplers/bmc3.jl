763098560b4e539fcdd0f6697ac029d0acaa7209
Benjamin Deonovic <bdeonovic@gmail.com>
2016-05-18 17:20:02 -0500

    Make BMC3 adaptive

--- src/samplers/bmc3.jl
+++ src/samplers/bmc3.jl
@@ -7,11 +7,18 @@ type BMC3Tune <: SamplerTune
   k::Int
   indexset::Vector{Vector{Int}}
 
+  n::Int
+  m::Vector{Float64}
+  v::Vector{Float64}
+
+  epsilon::Float64
+
   BMC3Tune() = new()
 
   BMC3Tune(x::Vector, logf::Nullable{Function}; k::Integer=1, 
-           indexset::Vector{Vector{Int}} = Vector{Vector{Int}}()) =
-    new(logf, k, indexset)
+           indexset::Vector{Vector{Int}} = Vector{Vector{Int}}(),
+           epsilon::Float64 = 1/length(x)) =
+    new(logf, k, indexset, 0, zeros(length(x)), zeros(length(x)), epsilon)
 end
 
 BMC3Tune(x::Vector; args...) =
@@ -32,11 +39,15 @@ end
 
 #################### Sampler Constructor ####################
 
-function BMC3(params::ElementOrVector{Symbol}; args...)
+function BMC3(params::ElementOrVector{Symbol}; adapt::Symbol = :none, args...)
+
+  adapt in [:var, :freq, :none] || 
+    throw(ArgumentError("adapt must be one of :var, :freq, or :none"))
+    
   samplerfx = function(model::Model, block::Integer)
     block = SamplingBlock(model, block)
     v = SamplerVariate(block; args...)
-    sample!(v, x -> logpdf!(block, x))
+    sample!(v, x -> logpdf!(block, x), adapt=adapt)
     relist(block, v)
   end
   Sampler(params, samplerfx, BMC3Tune())
@@ -45,15 +56,29 @@ end
 
 #################### Sampling Functions ####################
 
-sample!(v::BMC3Variate) = sample!(v, v.tune.logf)
+sample!(v::BMC3Variate; args...) = sample!(v, v.tune.logf; args...)
 
-function sample!(v::BMC3Variate, logf::Function)
+function sample!(v::BMC3Variate, logf::Function; adapt::Symbol = :none)
   x = v[:]
+
+  ## online calculation of mean and variance
+  v.tune.n += 1
+  d = x - v.tune.m
+  v.tune.m += d/v.tune.n
+  v.tune.v += d .* (x - v.tune.m)
+
   idx = Int64[]
-  if length(v.tune.indexset) > 0
-    idx = rand(v.tune.indexset)
+  if adapt == :var
+    idx = wsample(1:length(v), 
+            (1 - v.tune.epsilon) * (v.tune.v / (v.tune.n - 1)) + v.tune.epsilon)
+  elseif adapt == :freq
+    idx = wsample(1:length(v), (1 - v.tune.epsilon) * v.tune.m + v.tune.epsilon)
   else
-    idx = randperm(length(v))[1:v.tune.k]
+    if length(v.tune.indexset) > 0
+      idx = rand(v.tune.indexset)
+    else
+      idx = randperm(length(v))[1:v.tune.k]
+    end
   end
   x[idx] = 1.0 - v[idx]
   if rand() < exp(logf(x) - logf(v.value))
