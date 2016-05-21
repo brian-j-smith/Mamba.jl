b4ebad708a3fe4c0888df8c7443b59f76ac918ba
Brian J Smith <brian-j-smith@uiowa.edu>
2016-05-21 11:33:55 -0500

    Remove 0.8.1 deprecates.

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,24 +1,3 @@
-## Deprecated at 0.8.1
-
-function relist!{T<:Real}(m::Model, values::AbstractArray{T},
-                 nodekeys::Vector{Symbol}, transform::Bool=false)
-  msg = string("relist!{T<:Real}(m::Model, values::AbstractArray{T}, ",
-               "nodekeys::Vector{Symbol}, transform::Bool=false) is deprecated")
-  Base.depwarn(msg, :relist!)
-
-  x = relist(m, values, nodekeys, transform)
-  for key in nodekeys
-    m[key].value = x[key]
-  end
-  update!(m)
-end
-
-
-export tune
-
-@deprecate tune gettune
-
-
 ## Deprecated at 0.9.0
 
 export simulate!
