323a985184e97233d15bab25d8c08fa40f45f946
Benjamin Deonovic <bdeonovic@gmail.com>
2016-06-01 10:00:12 -0500

    update

--- src/samplers/bmc3.jl
+++ src/samplers/bmc3.jl
@@ -2,87 +2,130 @@
 
 #################### Types and Constructors ####################
 
-type BMC3Tune <: SamplerTune
-  logf::Nullable{Function}
-  k::Int
-  indexset::Vector{Vector{Int}}
+typealias BMC3Form Union{Int, Vector{Vector{Int}}}
 
+type BMC3Tune{F<:BMC3Form} <: SamplerTune
+  logf::Nullable{Function}
+  indices::F
   n::Int
   m::Vector{Float64}
   v::Vector{Float64}
-
   epsilon::Float64
 
   BMC3Tune() = new()
 
-  BMC3Tune(x::Vector, logf::Nullable{Function}; k::Integer=1, 
-           indexset::Vector{Vector{Int}} = Vector{Vector{Int}}(),
-           epsilon::Float64 = 1/length(x)) =
-    new(logf, k, indexset, 0, zeros(length(x)), zeros(length(x)), epsilon)
+  BMC3Tune(x::Vector, indices::F; args...) =
+    BMC3Tune{F}(x, indices, Nullable{Function}(); args...)
+
+  BMC3Tune(x::Vector, indices::F, logf::Function; args...) =
+    BMC3Tune{F}(x, indices, Nullable{Function}(logf); args...)
+
+  BMC3Tune(x::Vector, indices::F, logf::Nullable{Function};
+           epsilon::Real=1 / length(x)) =
+    new(logf, indices, 0, zeros(x), zeros(x), epsilon)
 end
 
-BMC3Tune(x::Vector; args...) =
-   BMC3Tune(x, Nullable{Function}(); args...)
 
-BMC3Tune(x::Vector, logf::Function; args...) =
-   BMC3Tune(x, Nullable{Function}(logf); args...)
+BMC3Variate(x::Vector; k::Integer=1, args...) =
+   SamplerVariate{BMC3Tune{Int}}(x, k; args...)
+
+BMC3Variate(x::Vector, logf::Function; k::Integer=1, args...) =
+   SamplerVariate{BMC3Tune{Int}}(x, k, logf; args...)
+
+BMC3Variate(x::Vector, indexset::Vector{Vector{Int}}; args...) =
+  SamplerVariate{BMC3Tune{Vector{Vector{Int}}}}(x, indexset; args...)
+
+BMC3Variate(x::Vector, indexset::Vector{Vector{Int}}, logf::Function; args...) =
+  SamplerVariate{BMC3Tune{Vector{Vector{Int}}}}(x, indexset, logf; args...)
 
 
-typealias BMC3Variate SamplerVariate{BMC3Tune}
-
-function validate(v::BMC3Variate)
+function validate(v::SamplerVariate{BMC3Tune{Int}})
   n = length(v)
-  v.tune.k <= n || throw(ArgumentError("k exceeds variate length $n"))
+  v.tune.indices <= n ||
+    throw(ArgumentError("indices exceed variate length $n"))
+  validatebinary(v)
+end
+
+function validate(v::SamplerVariate{BMC3Tune{Vector{Vector{Int}}}})
+  n = length(v)
+  mapreduce(maximum, max, v.tune.indices) <= n ||
+    throw(ArgumentError("indices exceed variate length $n"))
   validatebinary(v)
 end
 
 
 #################### Sampler Constructor ####################
 
-function BMC3(params::ElementOrVector{Symbol}; adapt::Symbol = :none, args...)
+function BMC3(params::ElementOrVector{Symbol}; k::Integer=1, args...)
+  BMC3Sampler(params, k; args...)
+end
 
-  adapt in [:var, :freq, :none] || 
-    throw(ArgumentError("adapt must be one of :var, :freq, or :none"))
-    
+function BMC3(params::ElementOrVector{Symbol}, indexset::Vector{Vector{Int}};
+              args...)
+  BMC3Sampler(params, indexset; args...)
+end
+
+function BMC3Sampler{F<:BMC3Form}(params::ElementOrVector{Symbol}, indices::F;
+                                  adapt::Symbol=:none, atype::Symbol=:var,
+                                  args...)
+  adapt in [:all, :burnin, :none] ||
+    throw(ArgumentError("adapt must be one of :all, :burnin, or :none"))
+  atype in [:var, :freq] ||
+    throw(ArgumentError("atype must be one of :var or :freq"))
+
   samplerfx = function(model::Model, block::Integer)
     block = SamplingBlock(model, block)
-    v = SamplerVariate(block; args...)
-    sample!(v, x -> logpdf!(block, x), adapt=adapt)
+    v = SamplerVariate(block, indices; args...)
+    isadapt = adapt == :burnin ? model.iter <= model.burnin :
+              adapt == :all ? true : false
+    sample!(v, x -> logpdf!(block, x), adapt=isadapt, atype=atype)
     relist(block, v)
   end
-  Sampler(params, samplerfx, BMC3Tune())
+  Sampler(params, samplerfx, BMC3Tune{F}())
 end
 
 
 #################### Sampling Functions ####################
 
-sample!(v::BMC3Variate; args...) = sample!(v, v.tune.logf; args...)
+sample!{F<:BMC3Form}(v::SamplerVariate{BMC3Tune{F}}; args...) =
+  sample!(v, v.tune.logf; args...)
 
-function sample!(v::BMC3Variate, logf::Function; adapt::Symbol = :none)
+function sample!{F<:BMC3Form}(v::SamplerVariate{BMC3Tune{F}}, logf::Function;
+                              adapt::Bool=false, atype::Symbol=:var)
   x = v[:]
-
-  ## online calculation of mean and variance
-  v.tune.n += 1
-  d = x - v.tune.m
-  v.tune.m += d/v.tune.n
-  v.tune.v += d .* (x - v.tune.m)
-
-  idx = Int64[]
-  if adapt == :var
-    idx = wsample(1:length(v), 
-            (1 - v.tune.epsilon) * (v.tune.v / (v.tune.n - 1)) + v.tune.epsilon)
-  elseif adapt == :freq
-    idx = wsample(1:length(v), (1 - v.tune.epsilon) * v.tune.m + v.tune.epsilon)
-  else
-    if length(v.tune.indexset) > 0
-      idx = rand(v.tune.indexset)
-    else
-      idx = randperm(length(v))[1:v.tune.k]
-    end
-  end
+  setadapt!(v, adapt)
+  idx = randind(v, atype)
   x[idx] = 1.0 - v[idx]
   if rand() < exp(logf(x) - logf(v.value))
     v[:] = x
   end
   v
 end
+
+function randind(v::SamplerVariate{BMC3Tune{Int}}, atype)
+  wv =  atype == :var ?
+        (1 - v.tune.epsilon) * (v.tune.v / (v.tune.n - 1)) + v.tune.epsilon :
+        (1 - v.tune.epsilon) * v.tune.m + v.tune.epsilon
+  sample(1:length(v), wv, v.tune.indices)
+end
+
+function randind(v::SamplerVariate{BMC3Tune{Vector{Vector{Int}}}})
+  wv =  atype == :var ?
+        (1 - v.tune.epsilon) * (v.tune.v / (v.tune.n - 1)) + v.tune.epsilon :
+        (1 - v.tune.epsilon) * v.tune.m + v.tune.epsilon
+  wvb = zeros(length(v.tune.indices))
+  for i in 1:length(v.tune.indices)
+    wvb[i] = mean(wv[v.tune.indices[i]])
+  end
+  sample(v.tune.indices, wvb)
+end
+
+function setadapt!(v::BMC3Variate, adapt::Bool)
+  if adapt
+    v.tune.n += 1
+    d = v.value - v.tune.m
+    v.tune.m += d / v.tune.n
+    v.tune.v += d .* (x - v.tune.m)
+  end
+end
+
