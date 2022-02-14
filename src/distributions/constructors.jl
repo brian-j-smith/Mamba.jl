3a4403f2944077096355ff60d78ba9762338cd3b
Brian J Smith <brian-j-smith@uiowa.edu>
2022-02-14 14:03:10 -0600

    Remove conflicting Distributions constructors

--- src/distributions/constructors.jl
+++ src/distributions/constructors.jl
@@ -12,12 +12,6 @@ Categorical(p::T; check_args = true) where {T <: ArrayVariate{1}} =
 # Distributions Package MultivariateDistribution
 ######################################################################
 
-#################### Dirichlet ####################
-
-Dirichlet(alpha::AbstractVector{T}) where {T<:Real} =
-  Dirichlet(convert(Vector{Float64}, alpha))
-
-
 #################### Multinomial ####################
 
 Multinomial(n::Real, p::AbstractVector{T}) where {T<:Real} =
@@ -42,9 +36,6 @@ MvNormalCanon(h::AbstractVector{T}, J::U) where {T<:Real, U<:AbstractMatrix} =
 
 MvNormalCanon(h::AbstractVector{T}, prec::U) where {T<:Real, U<:AbstractVector} =
   MvNormalCanon(convert(Vector{Float64}, h), convert(Vector{Float64}, prec))
-
-MvNormalCanon(J::AbstractMatrix{T}) where {T<:Real} =
-  MvNormalCanon(convert(Matrix{Float64}, J))
 
 MvNormalCanon(prec::AbstractVector{T}) where {T<:Real} =
   MvNormalCanon(convert(Vector{Float64}, prec))
